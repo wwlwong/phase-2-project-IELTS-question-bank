ee5553e86183ae21c67e67291c979edfeba8ec6c
William Wong <william.wai.lim.wong@gmail.com>
2023-06-26 16:58:20 -0400

    adds NavBar component

---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+  <div className="navbar">
+    <NavLink to="/">Home</NavLink>
+    <NavLink to='/speakingpart1'>Speaking Part 1</NavLink>
+    <NavLink to='/speakingpart2'>Speaking Part 2</NavLink>
+    <NavLink to='/speakingpart3'>Speaking Part 3</NavLink>
+  </div>
+  );
+}
+
+export default NavBar;
