45ce32099c97a8f7a8a6b77521c7891936d33892
William Wong <william.wai.lim.wong@gmail.com>
2023-06-27 16:54:52 -0400

    displays speaking part 2 questions

--- src/Speakingpart2.js
+++ src/Speakingpart2.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React,  { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function SpeakingPart2() {
-  return <div>
-    <h1>Speaking Part 2 Questions</h1>
-  </div>;
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("https://flatiron-phase-2-project.onrender.com/speaking_part2")
+      .then((r) => r.json())
+      .then((questions) => {
+        
+        setQuestions(questions)});
+  }, []); 
+  
+  
+  
+  return ( 
+    <div>
+    <section>
+      <h1>Speaking Part 2 Questions</h1>
+      <ol> 
+        {questions.map((question) => (
+        <QuestionItem key={question.id} question={question} /> ))}
+        
+      </ol>
+    </section>
+    ss
+  </div>
+  );
 }
 
 export default SpeakingPart2;
