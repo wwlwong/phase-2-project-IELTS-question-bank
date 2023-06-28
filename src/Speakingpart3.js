0639e798f80713e6654675dc774bf76f27a79ef1
William Wong <william.wai.lim.wong@gmail.com>
2023-06-27 20:58:47 -0400

    Removes useState from Speakingpart

--- src/Speakingpart3.js
+++ src/Speakingpart3.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import QuestionItem from "./QuestionItem";
 
 function SpeakingPart3({questions, setQuestions}) {
