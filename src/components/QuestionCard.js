871c6a0e44fc6c6dec00fc50c227d608727ac786
William Wong <william.wai.lim.wong@gmail.com>
2023-06-29 10:37:07 -0400

    adds QuestionCards.js

---
+++ src/components/QuestionCard.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function QuestionCard( {question}) {
+    const {id, topic, questions, views } = question;
+
+    const [flip, setFlip] = useState(false);
+
+
+    return (
+        <div className={`card ${flip ? "flip" : ""}`}>
+
+            <div className="topic" onClick={() => setFlip(!flip)}>
+                {topic}
+            </div>
+
+            <div className="questions" onClick={() => setFlip(!flip)}>
+                <ol>
+                    {questions.map((subquestion) => (
+                        <li>{subquestion}</li>
+                    ))}
+                </ol>
+            </div>
+        </div>
+
+    );
+}
+
+export default QuestionCard
