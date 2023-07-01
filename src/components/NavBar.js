25c207e37e45cb0f26ca427539adfdafcd5009a7
William Wong <william.wai.lim.wong@gmail.com>
2023-07-01 10:51:03 -0400

    adds styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "inline-block",
+  
+  width: "160px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "#6900ff",
+  textDecoration: "none",
+  textAlign: "center",
+  color: "white",
+};
+
 function NavBar() {
   return (
   <div className="navbar">
-    <NavLink to="/">Home</NavLink>
-    <NavLink to='/speakingpart1'>Speaking Part 1</NavLink>
-    <NavLink to='/speakingpart2'>Speaking Part 2</NavLink>
-    <NavLink to='/speakingpart3'>Speaking Part 3</NavLink>
-    <NavLink to='/QuestionForm'>Create New Question</NavLink>
+    <NavLink to="/" exact style={linkStyles} activeStyle={{background: "darkblue",}}>Home</NavLink>
+    <NavLink to='/speakingpart1' style={linkStyles}
+        activeStyle={{background: "darkblue",}}>Speaking Part 1</NavLink>
+    <NavLink to='/speakingpart2' style={linkStyles}
+        activeStyle={{ background: "darkblue",}}>Speaking Part 2</NavLink>
+    <NavLink to='/speakingpart3' style={linkStyles}
+        activeStyle={{background: "darkblue",}}>Speaking Part 3</NavLink>
+    <NavLink to='/QuestionForm' style={linkStyles}
+        activeStyle={{background: "darkblue",}}>Create New Question</NavLink>
   </div>
   );
 }
