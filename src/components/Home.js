098e59453bb6ee2a663a0b090130d0c88eeb3eb9
William Wong <william.wai.lim.wong@gmail.com>
2023-07-02 06:45:01 -0400

    fixes css properties for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,10 @@ import React from "react";
 function Home() {
   return( 
   <div className="home">
-    <h1>IELTS Speaking Prep</h1>
+    <h1 className="home-header">IELTS Speaking Prep</h1>
     <p className="home-body">This is a question bank for all 3 parts of IELTS Speaking Exam for the period of May to August 2023 </p>
+    <img className="home-image" src="https://upload.wikimedia.org/wikipedia/commons/0/0e/DP_IELTS_studies.png"></img>
+   
   </div>
   );
 }
